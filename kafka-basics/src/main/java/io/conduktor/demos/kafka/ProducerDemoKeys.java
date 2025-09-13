8d5b722d6c642f8c5f2a42d52b7367ccb56c73c1
muhammetatmaca <113444449+muhammetatmaca@users.noreply.github.com>
2025-09-13 23:06:49 +0300

    ConsumerDemo VE ConsumerDemoWithShutdown oluşturuldu.

---
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ProducerDemoKeys.java
@@ -0,0 +1,82 @@
+package io.conduktor.demos.kafka;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoKeys {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemoKeys.class.getSimpleName());
+
+    public static void main(String[] args) {
+        log.info("I am a Kafka Producer!");
+
+        // create Producer Properties
+        Properties properties = new Properties();
+/*
+// connect to Localhost
+//        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+
+// connect to Conduktor Playground
+properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+        properties.setProperty("security.protocol", "SASL_SSL");
+        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
+        properties.setProperty("sasl.mechanism", "PLAIN");
+*/
+
+        // connect to Localhost Kafka
+        properties.setProperty("bootstrap.servers", "localhost:19092");
+
+        // set producer properties
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        for (int j=0; j<2; j++){
+
+            for (int i=0; i<10; i++) {
+
+
+                String topic = "demo_java";
+                String key = "id_" + i;
+                String value = "hello world " + i;
+                // create a Producer Records
+                ProducerRecord<String, String> producerRecord =
+                        new ProducerRecord<>(topic, key, value);
+
+                // send data
+                producer.send(producerRecord, new Callback() {
+                    @Override
+                    public void onCompletion(RecordMetadata metadata, Exception e) {
+                        // executes every time a record successfully sent or an exception is thrown
+                        if (e == null) {
+                            // the record was successfully sent
+                            log.info("Received new metadata \n" +
+                                    "Topic: " + metadata.topic() + "\n" +
+                                    "key: " + key + "\n" +
+                                    "Partition: " + metadata.partition() + "\n" +
+                                    "Offset: " + metadata.offset() + "\n" +
+                                    "Timestamp: " + metadata.timestamp());
+                        } else {
+                            log.error("Error while producing", e);
+                        }
+                    }
+                });
+            }
+        }
+
+
+
+        // tell the producer to send all data and block until done -- synchronous
+        producer.flush();
+
+        // flush and close the producer
+        producer.close();
+    }
+}
