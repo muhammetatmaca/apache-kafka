9f344188c0160f613a38641d7aa5a4de35b1f67d
muhammetatmaca <113444449+muhammetatmaca@users.noreply.github.com>
2025-09-14 15:25:00 +0300

    WikimediaChange ile producer oluşturma

---
+++ kafka-producer-wikimedia/src/main/java/io/conduktor/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,46 @@
+package io.conduktor.demos.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventSource;
+import com.launchdarkly.eventsource.background.BackgroundEventHandler;
+import com.launchdarkly.eventsource.background.BackgroundEventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangesProducer {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        BackgroundEventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+
+        // Önce EventSource.Builder oluştur, sonra onu BackgroundEventSource.Builder'a ver
+        EventSource.Builder eventSourceBuilder = new EventSource.Builder(URI.create(url));
+        BackgroundEventSource.Builder builder = new BackgroundEventSource.Builder(eventHandler, eventSourceBuilder);
+        BackgroundEventSource eventSource = builder.build();
+
+
+        // start the producer in another thread
+        eventSource.start();
+
+        // we produce for 10 minutes and block the program until then
+        TimeUnit.MINUTES.sleep(10);
+
+
+    }
+}
